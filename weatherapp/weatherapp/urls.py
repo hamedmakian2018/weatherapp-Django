abe75f9a927eb8964c9669adc71454092ceaa60f
hamedmakian2018 <hamed.makian@gmail.com>
2020-09-14 01:39:51 +0300

    Add project files.

---
+++ weatherapp/weatherapp/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import include, url
+import core.views
+
+
+# Django processes URL patterns in the order they appear in the array
+urlpatterns = [
+    url(r'^$', core.views.home, name='index'),
+    url(r'^home$', core.views.home, name='home'),
+]
